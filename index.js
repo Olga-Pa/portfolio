e81613340b5d5b86f06fb2b02a5fab2708b66713
MacBook Pro <admin@MacBooks-MacBook-Pro.local>
2019-08-05 01:22:33 -0400

    add style

---
+++ index.js
@@ -0,0 +1,7 @@
+// window.addEventListener("load", function() {
+//   var selectors = document.querySelectorAll(".portret");
+//   for (var el of selectors) {
+//     el.classList.add('loaded');
+//     el.style.height = el.querySelector(".portret > *").scrollHeight + "px";
+//   }
+// }, false);
