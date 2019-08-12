261a04c0903206c2e4d471c72881efa978aa6909
MacBook Pro <admin@MacBooks-MacBook-Pro.local>
2019-08-11 23:59:40 -0400

    working

--- index.js
+++
@@ -1,7 +0,0 @@
-// window.addEventListener("load", function() {
-//   var selectors = document.querySelectorAll(".portret");
-//   for (var el of selectors) {
-//     el.classList.add('loaded');
-//     el.style.height = el.querySelector(".portret > *").scrollHeight + "px";
-//   }
-// }, false);
